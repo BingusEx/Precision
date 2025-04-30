af19751cb46a95bc44b0b67879fa5278c397057d
BingusEx <10480438+BingusEx@users.noreply.github.com>
2025-04-30 19:48:52 +0200

    Use plugin name instead

--- src/main.cpp
+++ src/main.cpp
@@ -49,10 +49,10 @@ namespace {
 #ifndef NDEBUG
 		const auto level = spdlog::level::trace;
 #else
-		const auto level = spdlog::level::info;
+		constexpr auto level = spdlog::level::info;
 #endif
 
-		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+		auto log = std::make_shared<spdlog::logger>("global"s, std::move(sink));
 		log->set_level(level);
 		log->flush_on(level);
 
@@ -89,7 +89,7 @@ SKSEPluginLoad(const LoadInterface* a_skse) {
 
 SKSEPluginInfo(
 	.Version = REL::Version{ 2, 0, 4, 0 },
-	.Name = "Precision",
+	.Name = Plugin::NAME,
 	.Author = "Ershin, Modified by BingusEx for the GTS Mod",
 	.StructCompatibility = SKSE::StructCompatibility::Independent,
 	.RuntimeCompatibility = SKSE::VersionIndependence::AddressLibrary
